50134641509c0d3761c55826aabbf16970068b50
mateusma1a <mateussm99@gmail.com>
2023-01-29 22:18:56 -0300

    fixed error on taks page and add autentication page

--- Todoit/urls.py
+++ Todoit/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from tasks.views import tasks
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tasks/', tasks, name='tasks'),
+    path('', include('autentication.urls')),
+    path('tasks/', include('tasks.urls')),
 ]
