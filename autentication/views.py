35fe914dac8298a403c62d880b65b891bafc6cd0
mateusma1a <mateussm99@gmail.com>
2023-01-29 22:35:25 -0300

    autentication page done

--- autentication/views.py
+++ autentication/views.py
@@ -3,5 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    context = {'name': 'World'}
-    return render(request, 'login.html', context)
+    return render(request, 'login.html')
